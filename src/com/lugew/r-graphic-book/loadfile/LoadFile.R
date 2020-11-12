e08bbd9adfbfd2086137c51ff54e6ecc3477a2c5
LuGew <lugew@outlook.com>
2020-11-12 17:06:32 +0800

    init

---
+++ src/com/lugew/r-graphic-book/loadfile/LoadFile.R
@@ -0,0 +1,24 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: LuGew
+# Created on: 2020/11/12
+
+#加载csv
+print("加载csv")
+students <- read.csv(
+  "D:/PythonWorkplace/R-graphic-cookbook/src/com/lugew/r-graphic-book/loadfile/student.csv",
+  stringsAsFactors = FALSE
+)
+
+print(students)
+
+
+print("加载xlsx")
+#加载xlsx
+studentsXlsx <- read.xlsx("D:/PythonWorkplace/R-graphic-cookbook/src/com/lugew/r-graphic-book/loadfile/studentsXlsx.xlsx")
+print(studentsXlsx)
+
+install.packages("xlsx")
+install.packages("xlsxjars")
+install.packages("rJava")
+Sys.setenv(JAVA_HOME='D:/JDK/jdk1.8.0_144/jre')
---
+++ src/com/lugew/r-graphic-book/test/Test.R
@@ -0,0 +1,8 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: LuGew
+# Created on: 2020/11/12
+
+string <- "hello world"
+
+print(string)
